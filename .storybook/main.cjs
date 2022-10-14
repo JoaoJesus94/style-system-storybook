31758474e0bbff3915c3bb53fa49caa200677fdc
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-10-14 09:20:14 +0100

    ci: add publish storybook flow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,21 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
+  // Needed when using github pages inside organization
+  // viteFinal: (config, { configType }) => {
+  //   if (configType === 'PRODUCTION') config.base = '/repo-name-inside-org'
+
+  //   return config
+  // },
 }
