5d48bef1c5311ea9912572cf2d5da602f755fefe
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-10-14 09:06:08 +0100

    initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: { children: 'Lorem ipsum.', size: 'md' },
+  argTypes: {
+    size: { options: ['sm', 'md', 'lg'], control: 'inline-radio' },
+    asChild: { table: { disable: true } },
+  },
+} as Meta<TextProps>
+
+export const Default: StoryObj = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: { size: 'sm' },
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: { size: 'lg' },
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: { asChild: true, children: <p>Text with p</p> },
+  argTypes: { children: { table: { disable: true } } },
+}
